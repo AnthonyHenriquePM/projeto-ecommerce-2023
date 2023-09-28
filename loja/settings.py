380ebb58d739e63bd88b8f9162c83ad169d9cf44
tonyhenri <anthonyhenriquecba8@gmail.com>
2023-09-28 17:04:35 -0400

    Criando models Produtos e Variação

--- loja/settings.py
+++ loja/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'produto',
 
     # TODO: Remover debug toolbar
     'debug_toolbar',
