160e4812b596ad35fd74d61db6228abcf832bba9
tonyhenri <anthonyhenriquecba8@gmail.com>
2023-10-03 23:22:19 -0400

    URLs

--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
 
-# Create your views here.
+
+class Criar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+
+class Atualizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+
+class Login(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+
+class Logout(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,29 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
 
-# Create your views here.
+
+class ListaProduto(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Lista')
+
+
+class DetalheProduto(View):
+    pass
+
+
+class AdicionarAoCarrinho(View):
+    pass
+
+
+class RemoverDoCarrinho(View):
+    pass
+
+
+class Carrinho(View):
+    pass
+
+
+class Finalizar(View):
+    pass
